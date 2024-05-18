bc5864f35ecc33faded839734b5097ae3a6c814d
mpgxc <mpgx5.c@gmail.com>
2024-05-18 15:12:35 -0300

    slices & loops

--- dive.go
+++ dive.go
@@ -48,4 +48,40 @@ func main() {
 	fmt.Printf("A: %T\n B: %T\n C: %T\n D: %T\n", A, B, C, D)
 	fmt.Printf("AA: %T\n BB: %T\n CC: %T\n DD: %T\n SS: %T\n", AA, BB, CC, DD, SS)
 	fmt.Printf("Point: %T\n", point)
+
+	// Arrays/Slices e Loops
+	var array [3]int
+
+	array[0] = 1
+	array[1] = 2
+	array[2] = 3
+
+	fmt.Println("\nArray size: ", len(array))
+	fmt.Println("\nArray first element: ", array[0])
+	fmt.Println("\nArray last element: ", array[len(array)-1])
+
+	for i := 0; i < len(array); i++ {
+		fmt.Println("Array element: ", array[i])
+	}
+
+	slice := []int{3, 2, 1}
+	slice_2 := []int{}
+	slice_3 := make([]int, 3)
+
+	for i, v := range slice {
+		fmt.Printf("Array element: %d, value: %d\n", i, v)
+	}
+
+	array_3 := slice[1:]
+
+	for i, v := range array_3 {
+		fmt.Printf("Array element: %d, value: %d\n", i, v)
+	}
+
+	fmt.Printf("Slice 1: %v -  Cap: %d\n", slice, cap(slice))
+	fmt.Printf("Slice 2: %v -  Cap: %d\n", slice_2, cap(slice_2))
+	fmt.Printf("Slice 3: %v -  Cap: %d\n", slice_3, cap(slice_3))
+
+	fmt.Println("\nSlice 1: ", slice[:2])
+	fmt.Println("\nSlice 1: ", slice[1:])
 }
