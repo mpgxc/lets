1fd9930342650c7aadde77c3717b60a6546ea618
mpgxc <mpgx5.c@gmail.com>
2024-05-18 19:18:59 -0300

    struct composition

--- structs.go
+++ structs.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 type User struct {
-	Name string
-	Age  int
+	Name    string
+	Age     int
+	Address Address
+}
+
+type Address struct {
+	City  string
+	State string
 }
 
 func handlerStructs() {
@@ -19,4 +25,24 @@ func handlerStructs() {
 	user.Age = 25
 
 	fmt.Println(user.Name, user.Age)
+
+	user.Address = Address{
+		City:  "Oeiras",
+		State: "Piauí",
+	}
+
+	fmt.Println(user.Address.City, user.Address.State)
+
+	user.Address = Address{
+		State: "Rio de Janeiro",
+	}
+
+	fmt.Println(user.Address.City, user.Address.State)
+
+	user2 := User{"JaneDoe", 30, Address{
+		City:  "Oeiras",
+		State: "Piauí",
+	}}
+
+	fmt.Println(user2.Name, user2.Age, user2.Address.City, user2.Address.State)
 }
